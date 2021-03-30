dc5207e58dbd31c4c7865b11ddf3f201ce628ac7
BoolAid-Man <marcouxaustin@gmail.com>
2021-03-29 21:11:23 -0400

    scaffolded CreateListing Activity

---
+++ app/src/main/java/com/marcoux/simplysale/CreateListingActivity.kt
@@ -0,0 +1,11 @@
+package com.marcoux.simplysale
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateListingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_listing)
+    }
+}
--- app/src/main/java/com/marcoux/simplysale/MainActivity.kt
+++ app/src/main/java/com/marcoux/simplysale/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.marcoux.simplysale
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.marcoux.simplysale.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+
+        val buttonListingsActivity = findViewById<Button>(R.id.buttonViewListings);
+        buttonListingsActivity.setOnClickListener {
+            val intent = Intent(this, ListingsActivity::class.java);
+            startActivity(intent);
+        }
+
+        val buttonCreateListingActivity = findViewById<Button>(R.id.buttonCreateListings);
+        buttonCreateListingActivity.setOnClickListener {
+            val intent = Intent(this, CreateListingActivity::class.java);
+            startActivity(intent);
+        }
     }
+
+
 }
