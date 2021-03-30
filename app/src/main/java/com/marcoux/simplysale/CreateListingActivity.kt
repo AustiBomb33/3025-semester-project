36176907d2540c0f663f6e2b0f5fa4098a68f211
BoolAid-Man <marcouxaustin@gmail.com>
2021-03-30 13:00:41 -0400

    CreateListing Activity can now go back to main

--- app/src/main/java/com/marcoux/simplysale/CreateListingActivity.kt
+++ app/src/main/java/com/marcoux/simplysale/CreateListingActivity.kt
@@ -2,10 +2,22 @@ package com.marcoux.simplysale
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.marcoux.simplysale.databinding.ActivityCreateListingBinding
+import com.marcoux.simplysale.databinding.ActivityMainBinding
 
 class CreateListingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateListingBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_listing)
+        binding = ActivityCreateListingBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+
+        val backButton = findViewById<Button>(R.id.backButton);
+        backButton.setOnClickListener {
+            finish();
+        }
     }
 }
