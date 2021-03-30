ce96ec51d2ed9c76889dea73fb1b57a6739e13b3
BoolAid-Man <marcouxaustin@gmail.com>
2021-03-30 19:37:24 -0400

    new view model that only returns listings owned by the current user

---
+++ app/src/main/java/com/marcoux/simplysale/ListingViewModelOwnerOnly.kt
@@ -0,0 +1,44 @@
+package com.marcoux.simplysale
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+
+class ListingViewModelOwnerOnly : ViewModel() {
+    private val listings = MutableLiveData<List<Listing>>()
+
+    init {
+        loadListings()
+    }
+
+    fun getListings(): LiveData<List<Listing>> {
+        return listings
+    }
+
+    private fun loadListings() {
+        val db = FirebaseFirestore.getInstance().collection("listings")
+            .orderBy("name", Query.Direction.ASCENDING)
+
+        db.addSnapshotListener { documents, exception ->
+            Log.i("DB_RESPONSE", "Returned ${documents?.size()} items")
+            if (exception != null) {
+                Log.w("DB_RESPONSE", exception)
+                return@addSnapshotListener
+            }
+
+            documents?.let {
+                val listingsTemp = ArrayList<Listing>()
+                for (document in documents) {
+                    val listing = document.toObject(Listing::class.java)
+                    if (listing.owner == FirebaseAuth.getInstance().currentUser!!.uid)
+                        listingsTemp.add(listing)
+                }
+                listings.value = listingsTemp
+            }
+        }
+    }
+}
