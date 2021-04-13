e910d75203b079ac7fb11e14b34ea0c1e7db0f3d
BoolAid-Man <marcouxaustin@gmail.com>
2021-04-13 15:47:38 -0400

    Main activity now selects a random listing to feature

--- app/src/main/java/com/marcoux/simplysale/MainActivity.kt
+++ app/src/main/java/com/marcoux/simplysale/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.marcoux.simplysale
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.observe
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 import com.marcoux.simplysale.databinding.ActivityMainBinding
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,6 +48,44 @@ class MainActivity : AppCompatActivity() {
         buttonModifyListing.setOnClickListener {
             val intent = Intent(this, OwnedListings::class.java)
             startActivity(intent)
+        }
+
+        val model: ListingViewModel by viewModels()
+        model.getListings().observe(this) { listings ->
+            val size = listings.size
+
+            Log.i("model size", "" + size)
+            val featured = model.getListings().value!![Random.nextInt(0, size)]
+            binding.featuredName.text = featured.name
+
+            val storage = FirebaseStorage.getInstance().reference
+            val imgURL = "images/${featured.image}.jpg"
+            Log.i("ImageURL", imgURL)
+            val storagePath = storage.child(imgURL)
+            storagePath.getBytes(1024 * 1024 * 1024).addOnSuccessListener { bytes ->
+                binding.featuredImage.setImageBitmap(
+                    BitmapFactory.decodeByteArray(
+                        bytes,
+                        0,
+                        bytes.size
+                    )
+                )
+            }
+
+            binding.featuredListing.setOnClickListener {
+                Log.i("Clicked listing", "${featured.name}")
+                val intent: Intent =
+                    if (featured.owner == FirebaseAuth.getInstance().currentUser!!.uid) {
+                        //modify the listing if user owns it
+                        Intent(this, ModifyListingActivity::class.java)
+                    } else {
+                        //otherwise, just view it
+                        Intent(this, ViewListingActivity::class.java)
+                    }
+                intent.putExtra("listing", featured)
+                startActivity(intent)
+            }
+
         }
     }
 
